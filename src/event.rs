80e044a60db395727ed72e6b18301d7b4d65a775
Emilia Bopp <contact@ebopp.de>
2015-01-14 16:22:15 +0100

    Rearrange reference bookkeeping
    Now everything is kept alive for the right amount of time, even when
    intermediaries get deallocated.

--- src/event.rs
+++ src/event.rs
@@ -3,6 +3,27 @@ use std::sync::{Arc, RwLock};
 use std::thread::Thread;
 use subject::Subject;
 
+/// Thread keeps internal subject alive
+fn spawn_over<A, B, F>(src: &Event<A>, dest: &Event<B>, f: F)
+    where F: Fn(&mut Subject<B>, A) + Send,
+          A: Send + Sync + Clone,
+          B: Send + Sync + Clone,
+{
+    let source = src.listen();
+    let subject = dest.subject.downgrade();
+    let keep_alive = src.subject.clone();
+    Thread::spawn(move || {
+        // This reference is only here to keep the subject observed by our
+        // source alive, as long as this thread is running.
+        let _ = keep_alive;
+        for a in source.iter() {
+            match subject.upgrade() {
+                Some(subject) => f(&mut *subject.write().unwrap(), a),
+                None => break,
+            }
+        }
+    });
+}
 
 pub struct Event<A> {
     subject: Arc<RwLock<Subject<A>>>,
@@ -21,27 +42,16 @@ impl<A: Send + Sync + Clone> Event<A> {
         self.subject.write().unwrap().listen()
     }
 
+
     pub fn map<B: Send + Sync + Clone, F: Fn(A) -> B + Send>(&self, f: F) -> Event<B> {
         let event = Event::new();
-        let source = self.listen();
-        let subject = event.subject.clone();
-        Thread::spawn(move || {
-            for a in source.iter() {
-                subject.write().unwrap().send(f(a))
-            }
-        });
+        spawn_over(self, &event, move |mut subject, a| subject.send(f(a)));
         event
     }
 
     pub fn filter<F: Fn(&A) -> bool + Send>(&self, f: F) -> Event<A> {
         let event = Event::new();
-        let source = self.listen();
-        let subject = event.subject.clone();
-        Thread::spawn(move || {
-            for a in source.iter().filter(|a| f(a)) {
-                subject.write().unwrap().send(a);
-            }
-        });
+        spawn_over(self, &event, move |mut subject, a| if f(&a) { subject.send(a) });
         event
     }
 
@@ -49,14 +59,8 @@ impl<A: Send + Sync + Clone> Event<A> {
     /// with the order, in which they were fired.
     pub fn merge(&self, other: &Event<A>) -> Event<A> {
         let event = Event::new();
-        for source in [self, other].iter().map(|x| x.listen()) {
-            let subject = event.subject.clone();
-            Thread::spawn(move || {
-                for a in source.iter() {
-                    subject.write().unwrap().send(a);
-                }
-            });
-        }
+        spawn_over(self, &event, |mut subject, a| subject.send(a));
+        spawn_over(other, &event, |mut subject, a| subject.send(a));
         event
     }
 }
@@ -102,6 +106,7 @@ mod test {
         sink1.send(3);
         sink2.send(4);
         let result = (r.recv(), r.recv());
+        // We can not be certain about the ordering
         assert!((result == (Ok(3), Ok(4))) || (result == (Ok(4), Ok(3))));
     }
 
@@ -112,5 +117,20 @@ mod test {
         let r = chain.listen();
         sink.send(9);
         assert_eq!(r.recv(), Ok(11));
+    }
+
+    #[test]
+    fn chain_more() {
+        let sink: Event<i32> = Event::new();
+        let chain = sink
+            .map(|x| x + 2)
+            .filter(|&x| x > 10)
+            .merge(&sink.filter(|&x| x < -3))
+            .map(|x| x - 4);
+        let r = chain.listen();
+        sink.send(9);
+        assert_eq!(r.recv(), Ok(7));
+        sink.send(-5);
+        assert_eq!(r.recv(), Ok(-9));
     }
 }
