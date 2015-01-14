d7704ae4601e1f9046eac95d7f5a31447d944ae1
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-01-14 18:25:10 +0100

    Restructure behaviour and implement snapshot
    Must live in the same module as Event, as snapshot needs to work with the
    internals of both.

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,18 +1,16 @@
 use std::sync::mpsc::Receiver;
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, Weak, RwLock};
 use std::thread::Thread;
 use subject::Subject;
-use Behaviour;
+
 
 /// Thread keeps internal subject alive
-fn spawn_over<A, B, F>(src: &Event<A>, dest: &Event<B>, f: F)
-    where F: Fn(&mut Subject<B>, A) + Send,
-          A: Send + Sync + Clone,
-          B: Send + Sync + Clone,
+fn spawn_over<A, B, C, F>(source: Receiver<A>, subject: Weak<RwLock<B>>, keep_alive: Arc<RwLock<C>>, f: F)
+    where F: Fn(&mut B, A) + Send,
+          A: Send + Sync,
+          B: Send + Sync,
+          C: Send + Sync,
 {
-    let source = src.listen();
-    let subject = dest.subject.downgrade();
-    let keep_alive = src.subject.clone();
     Thread::spawn(move || {
         // This reference is only here to keep the subject observed by our
         // source alive, as long as this thread is running.
@@ -25,6 +23,7 @@ fn spawn_over<A, B, F>(src: &Event<A>, dest: &Event<B>, f: F)
         }
     });
 }
+
 
 pub struct Event<A> {
     subject: Arc<RwLock<Subject<A>>>,
@@ -43,16 +42,23 @@ impl<A: Send + Sync + Clone> Event<A> {
         self.subject.write().unwrap().listen()
     }
 
+    fn spawn_over<B, F>(&self, subject: Weak<RwLock<B>>, f: F)
+        where F: Fn(&mut B, A) + Send,
+              B: Send + Sync,
+    {
+        spawn_over(self.listen(), subject, self.subject.clone(), f);
+    }
+
 
     pub fn map<B: Send + Sync + Clone, F: Fn(A) -> B + Send>(&self, f: F) -> Event<B> {
         let event = Event::new();
-        spawn_over(self, &event, move |mut subject, a| subject.send(f(a)));
+        self.spawn_over(event.subject.downgrade(), move |mut subject, a| subject.send(f(a)));
         event
     }
 
     pub fn filter<F: Fn(&A) -> bool + Send>(&self, f: F) -> Event<A> {
         let event = Event::new();
-        spawn_over(self, &event, move |mut subject, a| if f(&a) { subject.send(a) });
+        self.spawn_over(event.subject.downgrade(), move |mut subject, a| if f(&a) { subject.send(a) });
         event
     }
 
@@ -60,13 +66,54 @@ impl<A: Send + Sync + Clone> Event<A> {
     /// with the order, in which they were fired.
     pub fn merge(&self, other: &Event<A>) -> Event<A> {
         let event = Event::new();
-        spawn_over(self, &event, |mut subject, a| subject.send(a));
-        spawn_over(other, &event, |mut subject, a| subject.send(a));
+        self.spawn_over(event.subject.downgrade(), |mut subject, a| subject.send(a));
+        other.spawn_over(event.subject.downgrade(), |mut subject, a| subject.send(a));
         event
     }
 
     pub fn hold(&self, a: A) -> Behaviour<A> {
-        Behaviour::new(a, self)
+        Behaviour::hold(a, self)
+    }
+}
+
+
+pub struct Behaviour<A> {
+    state: Arc<RwLock<(A, Subject<A>)>>,
+}
+
+impl<A: Send + Sync + Clone> Behaviour<A> {
+    pub fn constant(initial: A) -> Behaviour<A> {
+        Behaviour {
+            state: Arc::new(RwLock::new((initial, Subject::new())))
+        }
+    }
+
+    pub fn hold(initial: A, event: &Event<A>) -> Behaviour<A> {
+        let behaviour = Behaviour::constant(initial);
+        event.spawn_over(
+            behaviour.state.downgrade(),
+            move |&mut (ref mut state, ref mut subject), a| {
+                *state = a.clone();
+                subject.send(a);
+            }
+        );
+        behaviour
+    }
+
+    pub fn snapshot<B: Send + Sync + Clone>(&self, event: &Event<B>) -> Event<(A, B)> {
+        let snap = Event::new();
+        let b_state = self.state.clone();
+        event.spawn_over(
+            snap.subject.downgrade(),
+            move |subject, a| {
+                subject.send((b_state.read().unwrap().0.clone(), a))
+            },
+        );
+        snap
+    }
+
+    pub fn sample(&self) -> A {
+        self.state.read().unwrap().0.clone()
     }
 }
 
@@ -111,8 +158,18 @@ mod test {
         sink1.send(3);
         sink2.send(4);
         let result = (r.recv(), r.recv());
-        // We can not be certain about the ordering
+        // We cannot be certain about the ordering
         assert!((result == (Ok(3), Ok(4))) || (result == (Ok(4), Ok(3))));
+    }
+
+    #[test]
+    fn snapshot() {
+        let sink = Event::new();
+        let behaviour = Event::new().hold(1);
+        let snap = behaviour.snapshot(&sink);
+        let r = snap.listen();
+        sink.send(4);
+        assert_eq!(r.recv(), Ok((1, 4)));
     }
 
     #[test]
