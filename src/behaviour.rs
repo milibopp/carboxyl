24a296fc506fed3660e80e1d0b7e5c42e70659e6
Emilia Bopp <contact@ebopp.de>
2015-01-14 15:07:52 +0100

    Implement basic behaviour

---
+++ src/behaviour.rs
@@ -0,0 +1,44 @@
+use std::sync::mpsc::Receiver;
+use std::sync::RwLock;
+use event::Event;
+
+pub struct Behaviour<A> {
+    state: RwLock<A>,
+    recv: Receiver<A>,
+}
+
+impl<A: Send + Sync + Clone> Behaviour<A> {
+    pub fn new(initial: A, event: &Event<A>) -> Behaviour<A> {
+        Behaviour { state: RwLock::new(initial), recv: event.listen() }
+    }
+
+    pub fn state(&self) -> A {
+        // Update internal state
+        match {
+            let mut new_state = None;
+            while let Ok(update) = self.recv.try_recv() {
+                new_state = Some(update);
+            }
+            new_state
+        } {
+            Some(update) => *self.state.write().unwrap() = update,
+            None => (),
+        }
+        self.state.read().unwrap().clone()
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use event::Event;
+    use super::*;
+
+    #[test]
+    fn behaviour_new() {
+        let sink = Event::new();
+        let behaviour = Behaviour::new(1, &sink);
+        sink.send(3);
+        assert_eq!(behaviour.state(), 3);
+    }
+}
