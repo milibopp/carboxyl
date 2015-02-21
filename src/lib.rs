79621c91882a3d22856d322fdd962e901b622917
Emilia Bopp <contact@ebopp.de>
2015-02-21 15:21:56 +0100

    Stop depending on the core compiler feature

--- src/lib.rs
+++ src/lib.rs
@@ -118,7 +118,7 @@
 //! functions to the FRP primitives, as they break the benefits you get from
 //! using FRP. (Except temporary print statements for debugging.)
 
-#![feature(core, alloc, std_misc, old_io, test)]
+#![feature(alloc, std_misc, old_io, test)]
 #![warn(missing_docs)]
 
 #[cfg(test)]
