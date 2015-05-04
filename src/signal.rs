ec09636daac55ecde9bdcfaa03f36e2028c347c9
Emilia Bopp <contact@ebopp.de>
2015-05-04 21:29:41 +0200

    Replace old implementation entirely

--- src/signal.rs
+++ src/signal.rs
@@ -65,7 +65,7 @@ pub fn signal_source<A>(signal: &Signal<A>) -> &Arc<Mutex<Source<()>>> {
 }
 
 
-/// A continuous signal.
+/// A continuous signal that changes over time.
 pub struct Signal<A> {
     current: Arc<Mutex<Pending<SignalFn<A>>>>,
     source: Arc<Mutex<Source<()>>>,
@@ -101,14 +101,33 @@ impl<A: Clone> Signal<A> {
         Signal::build(SignalFn::Const(a), ())
     }
 
-    /// Sample the current value of a signal.
+    /// Sample the current value of the signal.
     pub fn sample(&self) -> A {
         commit((), |_| self.current.lock().unwrap().call())
     }
 }
 
 impl<A: Clone + Send + Sync + 'static> Signal<A> {
-    /// stub
+    /// Combine the signal with a stream in a snapshot.
+    ///
+    /// `snapshot` creates a new stream given a signal and a stream. Whenever
+    /// the input stream fires an event, the output stream fires a pair of the
+    /// signal's current value and that event.
+    ///
+    /// ```
+    /// # use carboxyl::Sink;
+    /// let sink1: Sink<i32> = Sink::new();
+    /// let sink2: Sink<f64> = Sink::new();
+    /// let snapshot = sink1.stream().hold(1).snapshot(&sink2.stream());
+    /// let mut events = snapshot.events();
+    ///
+    /// // Updating its signal does not cause the snapshot to fire
+    /// sink1.send(4);
+    ///
+    /// // However sending an event down the stream does
+    /// sink2.send(3.0);
+    /// assert_eq!(events.next(), Some((4, 3.0)));
+    /// ```
     pub fn snapshot<B>(&self, stream: &Stream<B>) -> Stream<(A, B)>
         where B: Clone + Send + Sync + 'static,
     {
@@ -117,7 +136,74 @@ impl<A: Clone + Send + Sync + 'static> Signal<A> {
 }
 
 impl<A: Clone + Send + Sync + 'static> Signal<Signal<A>> {
-    /// stub
+    /// Switch between signals.
+    ///
+    /// This transforms a `Signal<Signal<A>>` into a `Signal<A>`. The nested
+    /// signal can be thought of as a representation of a switch between different
+    /// input signals, that allows one to change the structure of the dependency
+    /// graph at run-time. `switch` provides a way to access the inner value of
+    /// the currently active signal.
+    ///
+    /// The following example demonstrates how to use this to switch between two
+    /// input signals based on a `Button` event stream:
+    ///
+    /// ```
+    /// # use carboxyl::Sink;
+    /// // Button type
+    /// #[derive(Clone, Show)]
+    /// enum Button { A, B };
+    ///
+    /// // The input sinks
+    /// let sink_a = Sink::<i32>::new();
+    /// let sink_b = Sink::<i32>::new();
+    ///
+    /// // The button sink
+    /// let sink_button = Sink::<Button>::new();
+    ///
+    /// // Create the output
+    /// let output = {
+    ///
+    ///     // Hold input sinks in a signal with some initials
+    ///     let channel_a = sink_a.stream().hold(1);
+    ///     let channel_b = sink_b.stream().hold(2);
+    ///
+    ///     // A trivial default channel used before any button event
+    ///     let default_channel = Sink::new().stream().hold(0);
+    ///
+    ///     // Map button to the channel signals, hold with the default channel as
+    ///     // initial value and switch between the signals
+    ///     sink_button
+    ///         .stream()
+    ///         .map(move |b| match b {
+    ///             Button::A => channel_a.clone(),
+    ///             Button::B => channel_b.clone(),
+    ///         })
+    ///         .hold(default_channel)
+    ///         .switch()
+    /// };
+    ///
+    /// // In the beginning, output will come from the default channel
+    /// assert_eq!(output.sample(), 0);
+    ///
+    /// // Let's switch to channel A
+    /// sink_button.send(Button::A);
+    /// assert_eq!(output.sample(), 1);
+    ///
+    /// // And to channel B
+    /// sink_button.send(Button::B);
+    /// assert_eq!(output.sample(), 2);
+    ///
+    /// // The channels can change, too, of course
+    /// for k in 4..13 {
+    ///     sink_b.send(k);
+    ///     assert_eq!(output.sample(), k);
+    /// }
+    /// sink_button.send(Button::A);
+    /// for k in 21..77 {
+    ///     sink_a.send(k);
+    ///     assert_eq!(output.sample(), k);
+    /// }
+    /// ```
     pub fn switch(&self) -> Signal<A> {
         fn make_callback<A>(parent: &Signal<Signal<A>>) -> SignalFn<A>
             where A: Send + Clone + Sync + 'static,
@@ -139,19 +225,22 @@ impl<A: Clone + Send + Sync + 'static> Signal<Signal<A>> {
 }
 
 
-/// stub
+/// Forward declaration of a signal to create value loops.
+///
+/// This pattern is useful to implement accumulators, counters and other loops
+/// that depend on the sampling behaviour of a signal before a transaction.
 pub struct SignalCycle<A> {
     signal: Signal<A>,
 }
 
 impl<A: Send + Sync + Clone + 'static> SignalCycle<A> {
-    /// stub
+    /// Forward-declare a new signal.
     pub fn new() -> SignalCycle<A> {
         const ERR: &'static str = "sampled on forward-declaration of signal";
         SignalCycle { signal: Signal::build(SignalFn::from_fn(|| panic!(ERR)), ()) }
     }
 
-    /// stub
+    /// Provide the signal with a definition.
     pub fn define(self, definition: Signal<A>) -> Signal<A> {
         /// Generate a callback from the signal definition's current value.
         fn make_callback<A>(current_def: &Arc<Mutex<Pending<SignalFn<A>>>>) -> SignalFn<A>
