8e434a6d574abffd76651a9108bf9bf69f5ff4e2
Eduard Bopp <ebopp@thoughtworks.com>
2015-10-01 10:53:01 +0200

    Remove arc_weak feature, as it has been stabilized

--- src/lib.rs
+++ src/lib.rs
@@ -133,7 +133,7 @@
 //! the FRP primitives, as they break the benefits you get from using FRP.
 //! (An exception here is debugging output.)
 
-#![feature(arc_weak, fnbox)]
+#![feature(fnbox)]
 #![cfg_attr(test, feature(test))]
 #![warn(missing_docs)]
 
